08d3bda486ab16b49af0e52dc469013762d1e6ad
Muhammad Fahad <rfgul587@gmail.com>
2024-05-04 14:52:18 +0500

    the comment which was being genrated by background js was not being sent to the content script , so i moved the function which was generating comment to content script so now its working fully

--- background.js
+++ background.js
@@ -1,119 +1,96 @@
-<<<<<<< HEAD
-=======
-chrome.runtime.onMessage.addListener(async function (
-  request,
-  sender,
-  sendResponse
-) {
-  if (request.message === "startComment") {
-    try {
-      const title = await getTitleFromStorage();
-      if (!title) {
-        chrome.storage.local.get(["title"], (result) => {
-        console.log("The title of the video is: " + result.title);
-        return result.title;  
-        });
-      }
-      console.log("The title of the video is: " + title);
-      await generateComment(title);
-      sendResponse({ success: true }); // Sending success response
-    } catch (error) {
-      console.error("Error:", error);
-      sendResponse({ success: false, error: error.message }); // Sending error response
-    }
-  } else {
-    console.log("Running in background");
-  }
-});
+// chrome.runtime.onMessage.addListener(async function (
+//   request,
+//   sender,
+//   sendResponse
+// ) {
+//   if (request.action === "getTitle") {
+//     // try {
+     
+//     //     chrome.storage.local.get({"title": title}, (result) => {
+//     //       console.log("The title of the video is: " + result.title);
+//     //       return result.title;
+//     //     });
+      
+//     //   console.log("The title of the video is: " + title);
+//      const title = request.title;
+//      const res = await generateComment(title);
+//      chrome.runtime.sendMessage({ message: res });
+//      console.log("Comment:", res);
 
-async function getTitleFromStorage() {
-  // Retrieve the title from local storage
-  chrome.storage.local.get(["title"], (result) => {
-    console.log("The title of the video is: " + result.title);
-    return result.title;
-  });
-}
+//     //   sendResponse({ success: true, comment: res }); // Sending success response
+      
+//     // } catch (error) {
+//     //   console.log("Error:", error);
+//     //   sendResponse({ success: false, error: error.message }); // Sending error response
+//     // }
+//   } else {
+//     console.log("Running in background");
+//   }
+// });
 
->>>>>>> parent of 5713e1e (Revert "almost half task is done , the comment is beign generated")
-async function generateComment(title) {
-  const apiKey = "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";
-  const apiUrl = "https://api.groq.com/openai/v1/chat/completions";
-  const messages = [
-    {
-      role: "system",
-      content:
-        "You are a content writer who has to comment on the YouTube videos based on the title given to you, but it should  look like a generic human comment . It should not be more than 50 words and 100 characters.",
-    },
-    {
-      role: "user",
-      content: title,
-    },
-  ];
+// // async function getTitleFromStorage() {
 
-<<<<<<< HEAD
-  try {
-    const response = await fetch(apiUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "response-format": "json_object",
-        Authorization: `Bearer ${apiKey}`,
-      },
-      body: JSON.stringify({ messages, model: "mixtral-8x7b-32768" }),
-    });
+// //     chrome.storage.local.get({"title": title}, (result) => {
+// //       if (chrome.runtime.lastError) {
+// //         reject(chrome.runtime.lastError);
+// //       } else {
+// //       return result.title;
+// //       }
+// //     });
+  
+// // }
 
-    console.log(response);
-    if (!response.ok) {
-      throw new Error(
-        `Network response was not ok: ${response.statusText}`
-      );
-    }
+// async function generateComment(title) {
+//   const apiKey = "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";
+//   // const apiKey = "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";
+//   // const apiKey = "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";
+//   const apiUrl = "https://api.groq.com/openai/v1/chat/completions";
+//   const messages = [
+//     {
+//       role: "system",
+//       content:
+//         "You are a content writer who has to comment on the YouTube videos based on the title given to you, but it should  look like a generic human comment . It should not be more than 50 words and 100 characters.",
+//     },
+//     {
+//       role: "user",
+//       content: title,
+//     },
+//   ];
 
-    const data = await response.json();
-    console.log(data);
-    const comment = data.choices[0].message.content;
-    console.log("Generated comment:", comment);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-=======
-  console.log("title", title); // Moved here for logging
+//   try {
+//     const response = await fetch(apiUrl, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "response-format": "json_object",
+//         Authorization: `Bearer ${apiKey}`,
+//       },
+//       body: JSON.stringify({ messages, model: "mixtral-8x7b-32768" }),
+//     });
 
-  const response = await fetch(apiUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "response-format": "json_object",
-      Authorization: `Bearer ${apiKey}`,
-    },
-    body: JSON.stringify({ messages, model: "mixtral-8x7b-32768" }),
-  });
+//     console.log(response);
+//     if (!response.ok) {
+//       throw new Error(
+//         `Network response was not ok: ${response.statusText}`
+//       );
+//     }
 
-  if (!response.ok) {
-    throw new Error(`Network response was not ok: ${response.statusText}`);
-  }
+//     const data = await response.json();
+//     console.log(data);
+//     const comment = data.choices[0].message.content;
+//     console.log("Generated comment:", comment);
+//     return comment;
+ 
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//   }
+// }
 
-  const data = await response.json();
-  console.log(data);
-  const comment = data.choices[0].message.content;
-  console.log("Generated comment:", comment);
 
-  // Send the comment to the content script
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: (comment) => {
-      // This function will be executed in the context of the content script
-      // Inject the comment into the DOM or wherever it needs to be used
-      console.log("Received comment:", comment);
-      // Example: Inject the comment into a textarea
-      const textarea = document.querySelector("textarea");
-      if (textarea) {
-        textarea.value = comment;
-      }
-    },
-    args: [comment], // Pass the comment as an argument
-  });
->>>>>>> parent of 5713e1e (Revert "almost half task is done , the comment is beign generated")
-}
 
-// background.js
+
+
+
+
+
+
--- content.js
+++ content.js
@@ -1,108 +1,242 @@
-chrome.runtime.onMessage.addListener(function (request) {
+// chrome.runtime.onMessage.addListener(function (request) {
+//   if (request.message === "startProcess") {
+//     console.log("Process started from content.js");
+//     console.log("response sent from content.js");
+
+//     let videoSelector = document.querySelectorAll(
+//       "#thumbnail > yt-image > img"
+//     )[1];
+//     console.log("vido selector", videoSelector);
+//     videoSelector.click();
+
+//     setTimeout(function (sendMessage) {
+//       const titleElement = document.querySelector(
+//         "#title yt-formatted-string"
+//       ).innerText;
+//       console.log(titleElement);
+//       chrome.runtime.sendMessage({
+//         action: "getTitle",
+//         title: titleElement,
+//       });
+//      const res = await generateComment(title);
+// async function generateComment(title) {
+//   const apiKey = "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";
+//   // const apiKey = "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";
+//   // const apiKey = "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";
+//   const apiUrl = "https://api.groq.com/openai/v1/chat/completions";
+//   const messages = [
+//     {
+//       role: "system",
+//       content:
+//         "You are a content writer who has to comment on the YouTube videos based on the title given to you, but it should  look like a generic human comment . It should not be more than 50 words and 100 characters.",
+//     },
+//     {
+//       role: "user",
+//       content: title,
+//     },
+//   ];
+
+//   try {
+//     const response = await fetch(apiUrl, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "response-format": "json_object",
+//         Authorization: `Bearer ${apiKey}`,
+//       },
+//       body: JSON.stringify({ messages, model: "mixtral-8x7b-32768" }),
+//     });
+
+//     console.log(response);
+//     if (!response.ok) {
+//       throw new Error(`Network response was not ok: ${response.statusText}`);
+//     }
+
+//     const data = await response.json();
+//     console.log(data);
+//     const comment = data.choices[0].message.content;
+//     console.log("Generated comment:", comment);
+//     return comment;
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//   }
+// }
+
+//       // like the video
+//       const buttonViewModel = document.querySelector("button-view-model");
+
+//       if (buttonViewModel) {
+//         const likeButton = buttonViewModel.querySelector("button");
+
+//         if (likeButton) {
+//           likeButton.click();
+//         } else {
+//           console.error("Like button not found within button-view-model");
+//         }
+//       } else {
+//         console.error("button-view-model element not found");
+//       }
+
+//       // Scroll
+//       window.scrollBy(0, 400);
+//       console.log("Scrolled");
+//       let subscribeButton = document.querySelector(
+//         "#subscribe-button-shape > button"
+//       );
+//       subscribeButton.click();
+
+//       var placeholderInterval = setInterval(function () {
+//         // chrome.runtime.onMessage.addListener(function(request){
+//         var placeholder = document.getElementById("placeholder-area");
+//         if (placeholder) {
+//           clearInterval(placeholderInterval);
+//           placeholder.focus();
+//           placeholder.click();
+//           document.execCommand("insertText", true, "nice video");
+
+//           // document.execCommand("insertText", true, "nice video");
+
+//           // Listen for message from the background script
+
+//           console.log(request.message);
+
+//           // Wait for submit button to be available
+//           var buttonInterval = setInterval(function () {
+//             var button = document.getElementById("submit-button");
+//             if (button) {
+//               clearInterval(buttonInterval);
+//               button.click();
+//             } else {
+//               console.log("Button with ID 'submit-button' not found.");
+//             }
+//           }, 1000); // Check every second for the submit button
+//         } else {
+//           console.log("Element with ID 'placeholder-area' not found.");
+//         }
+//       }, 1000); // Check every second for the placeholder area
+//     }, 3000); // Wait for 2 seconds after subscription click
+//     // });
+//   }
+// });
+
+chrome.runtime.onMessage.addListener(async function (request) {
   if (request.message === "startProcess") {
     console.log("Process started from content.js");
-    //sendResponse({ message: "Process started" });
     console.log("response sent from content.js");
 
     let videoSelector = document.querySelectorAll(
       "#thumbnail > yt-image > img"
-    )[0];
-    console.log("vido selector", videoSelector);
+    )[4];
+    console.log("video selector", videoSelector);
     videoSelector.click();
 
-    setTimeout(function () {
-      // copying the title of the video
+    setTimeout(async function () {
+      const titleElement = document.querySelector(
+        "#title yt-formatted-string"
+      ).innerText;
+      console.log(titleElement);
 
-      // Find the yt-formatted-string element directly
-      const titleElement = document.querySelector("#title yt-formatted-string");
+      try {
+        const comment = await generateComment(titleElement);
+        console.log("Generated comment:", comment);
 
-      // Check if the title element is found and has text content
-      if (titleElement && titleElement.textContent) {
-        // Get the text content of the yt-formatted-string element
-        const title = titleElement.textContent.trim();
-        // alert(title);
-        // Log the title
-        // console.log("Title of the video:", title);
-
-        // Set the title in the local storage
-        // Set the title in local storage
-        chrome.storage.local.set({ title: title }, () => {
-          console.log("The title of the video is: " + title);
+        // Send the comment to the extension's background script
+        chrome.runtime.sendMessage({
+          action: "comment",
+          comment: comment,
         });
 
-        // Retrieve the title from local storage
-        chrome.storage.local.get(["title"], (result) => {
-          console.log("The title of the video is: " + result.title);
-        });
-      }
+        // Like the video
+        const buttonViewModel = document.querySelector("button-view-model");
 
-      // like the video
-      const buttonViewModel = document.querySelector("button-view-model");
+        if (buttonViewModel) {
+          const likeButton = buttonViewModel.querySelector("button");
 
-      // Check if the button-view-model element is found
-      if (buttonViewModel) {
-        // Get the underlying button element
-        const likeButton = buttonViewModel.querySelector("button");
-
-        // Check if the like button is found
-        if (likeButton) {
-          // Simulate a click event on the like button
-          likeButton.click();
+          if (likeButton) {
+            likeButton.click();
+          } else {
+            console.error("Like button not found within button-view-model");
+          }
         } else {
-          console.error("Like button not found within button-view-model");
+          console.error("button-view-model element not found");
         }
-      } else {
-        console.error("button-view-model element not found");
-      }
 
-      // Scroll
-      window.scrollBy(0, 400);
-      console.log("Scrolled");
-      let subscribeButton = document.querySelector(
-        "#subscribe-button-shape > button"
-      );
-      subscribeButton.click();
-      // Wait for subscription action to complete
+        // Scroll
+        window.scrollBy(0, 450);
+        console.log("Scrolled");
 
-      // Wait for placeholder area to be available
-      var placeholderInterval = setInterval(function () {
-        var placeholder = document.getElementById("placeholder-area");
-        if (placeholder) {
-          clearInterval(placeholderInterval);
-          placeholder.focus();
-          placeholder.click();
-          // document.execCommand("insertText", true, "nice video");
+        let subscribeButton = document.querySelector(
+          "#subscribe-button-shape > button"
+        );
+        subscribeButton.click();
 
-          // Listen for message from the background script
-          chrome.runtime.onMessage.addListener(
-            (message, sender, sendResponse) => {
-              // Check if the message is the comment
-              if (message.comment) {
-                // Use the received comment, for example, inject it into a textarea
-                const textarea = document.querySelector("textarea");
-                if (textarea) {
-                  textarea.value = message.comment;
-                }
+        var placeholderInterval = setInterval(function () {
+          var placeholder = document.getElementById("placeholder-area");
+          if (placeholder) {
+            clearInterval(placeholderInterval);
+            placeholder.focus();
+            placeholder.click();
+            document.execCommand("insertText", true, comment);
+
+            // Listen for message from the background script
+
+            console.log(request.message);
+
+            // Wait for submit button to be available
+            var buttonInterval = setInterval(function () {
+              var button = document.getElementById("submit-button");
+              if (button) {
+                clearInterval(buttonInterval);
+                button.click();
+              } else {
+                console.log("Button with ID 'submit-button' not found.");
               }
-            }
-          );
-
-          // Wait for submit button to be available
-          var buttonInterval = setInterval(function () {
-            var button = document.getElementById("submit-button");
-            if (button) {
-              clearInterval(buttonInterval);
-              button.click();
-            } else {
-              console.log("Button with ID 'submit-button' not found.");
-            }
-          }, 1000); // Check every second for the submit button
-        } else {
-          console.log("Element with ID 'placeholder-area' not found.");
-        }
-      }, 1000); // Check every second for the placeholder area
-    }, 3000); // Wait for 2 seconds after subscription click
-    // alert("The title of the video is: " + title);
+            }, 1000); // Check every second for the submit button
+          } else {
+            console.log("Element with ID 'placeholder-area' not found.");
+          }
+        }, 1000); // Check every second for the placeholder area
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }, 3000); // Wait for 3 seconds after subscription click
   }
 });
-chrome.runtime.sendMessage({ message: "startComment" });
+
+async function generateComment(title) {
+  const apiKey = "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";
+  const apiUrl = "https://api.groq.com/openai/v1/chat/completions";
+  const messages = [
+    {
+      role: "system",
+      content:
+        "You are a content writer who has to comment on the YouTube videos based on the title given to you, but it should look like a generic human comment. It should not be more than 50 words and 100 characters.",
+    },
+    {
+      role: "user",
+      content: title,
+    },
+  ];
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "response-format": "json_object",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify({ messages, model: "mixtral-8x7b-32768" }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    const comment = data.choices[0].message.content;
+    return comment;
+  } catch (error) {
+    throw new Error("Error fetching data:", error);
+  }
+}
